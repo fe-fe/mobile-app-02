0ea64527cd18fc842b2372ff55368899804922b3
LucasSantanaCWB <lucas@sisand.com.br>
2026-04-27 22:32:46 -0300

    implementação da chamada para tela de edição

--- app/src/main/java/br/ufpr/agenda/MainActivity.kt
+++ app/src/main/java/br/ufpr/agenda/MainActivity.kt
@@ -1,19 +1,23 @@
 package br.ufpr.agenda
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.ufpr.agenda.data.dao.TarefaDAO
+import br.ufpr.agenda.data.model.TarefaModel
 
 class MainActivity : AppCompatActivity() {
-    lateinit var  listView: ListView
-    lateinit var  adapter: ArrayAdapter<String>
-    val lista = mutableListOf<String>()
+    lateinit var listView: ListView
+    lateinit var adapter: ArrayAdapter<TarefaModel>
+    val lista = mutableListOf<TarefaModel>()
     lateinit var tarefaDAO: TarefaDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,16 +30,6 @@ class MainActivity : AppCompatActivity() {
 
         tarefaDAO = TarefaDAO(this)
 
-        tarefaDAO.create(
-            br.ufpr.agenda.data.model.TarefaModel(
-                id = 0,
-                titulo = "Tarefa teste",
-                descricao = "Teste banco",
-                id_status = 1,
-                id_prioridade = 1
-            )
-        )
-
         listView = findViewById(R.id.tasksListView)
         adapter = ArrayAdapter(
             this,
@@ -43,17 +37,31 @@ class MainActivity : AppCompatActivity() {
             lista
         )
         listView.adapter = adapter
+
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val tarefa = lista[position]
+            val intent = Intent(this, TarefaActivity::class.java)
+            intent.putExtra("TAREFA_ID", tarefa.id)
+            startActivity(intent)
+        }
+
+        // Clique para ADIÇÃO
+        val btnAdd: Button = findViewById(R.id.addTaskButton)
+        btnAdd.setOnClickListener {
+            val intent = Intent(this, TarefaActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
         carregarTarefas()
     }
-        fun carregarTarefas() {
-            lista.clear()
 
-            val tarefas = tarefaDAO.find()
-
-            for (t in tarefas) {
-                val texto = "${t.titulo} | Status: ${t.id_status} | Prioridade: ${t.id_prioridade}"
-                lista.add(texto)
-            }
-            adapter.notifyDataSetChanged()
-        }
+    fun carregarTarefas() {
+        lista.clear()
+        val tarefas = tarefaDAO.find()
+        lista.addAll(tarefas)
+        adapter.notifyDataSetChanged()
+    }
 }
